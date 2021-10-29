c0886e32f0982c11a1cce4934b938606b144631b
Paul Feuvraux <pfeuvraux@gmail.com>
2021-10-29 21:48:46 +0200

    fmt + final

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use argparse;
 mod proc;
 
 fn main() {
-
     let mut kill_processes: bool = false;
 
     {
@@ -10,9 +9,11 @@ fn main() {
         argparser.set_description("List and kill zombie processes.");
 
         // to kill or not to kill
-        argparser.refer(&mut kill_processes)
-            .add_option(&["-k", "--kill"], argparse::StoreTrue,
-                "Enable killing processes.");
+        argparser.refer(&mut kill_processes).add_option(
+            &["-k", "--kill"],
+            argparse::StoreTrue,
+            "Enable killing processes.",
+        );
 
         argparser.parse_args_or_exit();
     }
@@ -20,10 +21,7 @@ fn main() {
     let mut zombie_procs = proc::ZombiesProcs::new();
     zombie_procs.list_zombies();
 
-
     if kill_processes {
         zombie_procs.kill_processes();
     }
 }
-
-
--- src/proc.rs
+++ src/proc.rs
@@ -1,58 +1,67 @@
-use sysinfo::{self, SystemExt, ProcessExt};
 use std::process::Command;
+use std::str;
+use sysinfo::{self, ProcessExt, SystemExt};
 
-pub struct ZombieProc {
-  pid: i32,
-  ppids: Vec<i32>
+struct ZombieProc {
+    ppids: Vec<i32>,
 }
 
 pub struct ZombiesProcs {
-  procs: Vec<ZombieProc>
+    procs: Vec<ZombieProc>,
 }
 
 impl ZombiesProcs {
-  pub fn new() -> ZombiesProcs {
-    ZombiesProcs{
-      procs: Vec::new()
+    pub fn new() -> ZombiesProcs {
+        ZombiesProcs { procs: Vec::new() }
     }
-  }
 
-  pub fn list_zombies(&mut self) {
-    let mut sys = sysinfo::System::new_all();
-    sys.refresh_all();
+    pub fn list_zombies(&mut self) {
+        let mut sys = sysinfo::System::new_all();
+        sys.refresh_all();
 
-    for (&pid, proc) in sys.processes() {
-        println!("{:?}", proc);
-        let proc_state: String = proc.status().to_string();
-        if proc_state== "Zombie" {
-          let mut ppids: Vec<i32> = Vec::new();
+        for (&pid, proc) in sys.processes() {
+            let proc_state: String = proc.status().to_string();
+            if proc_state == "Zombie" {
+                let zombie = ZombieProc {
+                    ppids: list_ppids(pid),
+                };
 
-          match proc.parent() {
-            Some(i) => ppids.push(i),
-            _ => (),
-          }
-
-          let zombie = ZombieProc{
-            pid: pid,
-            ppids: ppids,
-          };
-
-          self.procs.push(zombie)
+                self.procs.push(zombie)
+            }
         }
     }
-  }
 
-
-  pub fn kill_processes(&self) {
-    for proc in &self.procs {
-      println!("Going on PID: {}", proc.pid);
-      println!("{:?}", proc.ppids);
-      for ppid in &proc.ppids {
-        println!("Killing PPID: {}", ppid);
-        Command::new("kill")
-          .args(["-9", &ppid.to_string()]);
-      }
+    pub fn kill_processes(&self) {
+        for proc in &self.procs {
+            for ppid in &proc.ppids {
+                println!("Killing PPID: {}", ppid);
+                Command::new("kill")
+                    .args(["-9", &ppid.to_string()])
+                    .spawn()
+                    .unwrap();
+            }
+        }
     }
-  }
+}
 
+fn list_ppids(pid: i32) -> Vec<i32> {
+    let mut ppids: Vec<i32> = Vec::new();
+
+    let pid_str_arg: String = format!("ppid={}", pid.to_string());
+    let raw_ppid_output = Command::new("ps")
+        .args(["-o", &pid_str_arg])
+        .output()
+        .expect("Failed to execute ps command.");
+
+    let str_ppid_output: &str = match str::from_utf8(&raw_ppid_output.stdout) {
+        Ok(output) => output,
+        Err(_) => panic!("Couldn't convert stdout to string."),
+    };
+
+    for ppid in str_ppid_output.lines() {
+        let p: i32 = ppid.trim().parse().unwrap();
+        ppids.push(p);
+    }
+
+    ppids
 }
